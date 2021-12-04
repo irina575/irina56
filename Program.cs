cd6af2ac48fc814741fcc8884006409ec3b9fb4d
irina575 <95540279+irina575@users.noreply.github.com>
2021-12-04 19:13:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x+y);
+            Console.ReadKey();
+        }
+    }
+}
